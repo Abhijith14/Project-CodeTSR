80866b8337a377d3bd6b0c0552aea567f52c89f2
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2023-03-11 11:31:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from davinci import output as davinci_output
 from linkedin_api import output as linkedin_output
 
 prompt = ("A LinkedIn post on one interesting python hack to make your life easier. Include hash tags.")
-print(davinci_output(prompt))
 
+answer = davinci_output(prompt)
 
-linkedin_output(davinci_output(prompt))
+print(answer)
+
+linkedin_output(answer)
 # linkedin_output("hello world", "https://abhijith14.github.io/Portfolio-v2/media/buddyai.png")
